68eeb011fd80186a7d2ed1cc7ac103351304f2fb
tbaig <mbaig91@gmail.com>
2017-03-06 14:39:27 -0800

    Added Express web server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./build'));
+
+app.get('/*', function(req, res){
+	res.sendFile(path.join(__dirname, './build', 'index.html'));
+})
+
+app.listen(8080, function(){
+	console.log("App is live on part 3000");
+})
